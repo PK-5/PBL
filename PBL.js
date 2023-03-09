d3bd526a504e9e4bb15f17627ae86727f0e12230
PK-5 <karumalil.p@maristcollege.school.nz>
2023-03-10 10:39:33 +1300

    open and close buttons

---
+++ PBL.js
@@ -0,0 +1,11 @@
+function openNav() {
+  document.getElementById("navigation").style.display = "block";
+  document.getElementById("opnbtn").style.display = "none";
+
+}
+
+function closeNav() {
+  document.getElementById("navigation").style.display = "none";
+  document.getElementById("opnbtn").style.display = "block";
+
+}
