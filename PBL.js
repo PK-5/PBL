1ac58da50f3f52327d01167cc5b95c195c01fad4
PK-5 <karumalil.p@maristcollege.school.nz>
2023-03-16 10:02:55 +1300

    Insertering logo and laying out the index page

--- PBL.js
+++ PBL.js
@@ -1,11 +1,13 @@
 function openNav() {
   document.getElementById("navigation").style.display = "block";
   document.getElementById("header").style.display= "none";
+  document.getElementById("pgbody").style.display= "none";
 
 }
 
 function closeNav() {
   document.getElementById("navigation").style.display = "none";
   document.getElementById("header").style.display = "block";
+  document.getElementById("pgbody").style.display= "block";
 
 }
