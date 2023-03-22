495eb23a31e7b0de08806e42fd324f5a9a1ce52e
PK-5 <karumalil.p@maristcollege.school.nz>
2023-03-22 21:18:21 +1300

    Adding subpages and changing layout of index page

--- PBL.js
+++ PBL.js
@@ -1,13 +1,20 @@
+
+
 function openNav() {
   document.getElementById("navigation").style.display = "block";
-  document.getElementById("header").style.display= "none";
+  document.getElementById("header").style.display = "none";
   document.getElementById("pgbody").style.display= "none";
-
+  document.getElementById("footer").style.display= "none";
 }
+
+
+
+
 
 function closeNav() {
   document.getElementById("navigation").style.display = "none";
   document.getElementById("header").style.display = "block";
   document.getElementById("pgbody").style.display= "block";
+  document.getElementById("footer").style.display= "block";
 
 }
